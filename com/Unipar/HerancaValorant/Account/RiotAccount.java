277c016f8afb7fe984e2531ae07bf40a7c2cd8db
Alan Gabriel Giacomoni Przyvara <88240260+YiipHuhu@users.noreply.github.com>
2024-04-30 21:08:04 -0300

    Add files via upload

---
+++ com/Unipar/HerancaValorant/Account/RiotAccount.java
@@ -0,0 +1,8 @@
+package com.Unipar.HerancaValorant.Account;
+
+public class RiotAccount {
+    private String riotID;
+    private int jogo;
+    // 1- com.Unipar.HerancaValorant.Account.Valorant, 2-Teamfight tactics, 3-Legends of Runneterra, 4- League of Legends.
+
+}

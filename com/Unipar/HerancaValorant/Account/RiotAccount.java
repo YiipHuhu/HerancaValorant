1e02936551fdf17a2a5726a1a6eaeff81550da6b
Alan Gabriel Giacomoni Przyvara <88240260+YiipHuhu@users.noreply.github.com>
2024-05-01 01:28:26 -0300

    Atualizar o RiotAccount.java

--- com/Unipar/HerancaValorant/Account/RiotAccount.java
+++ com/Unipar/HerancaValorant/Account/RiotAccount.java
@@ -3,6 +3,6 @@ package com.Unipar.HerancaValorant.Account;
 public class RiotAccount {
     private String riotID;
     private int jogo;
-    // 1- com.Unipar.HerancaValorant.Account.Valorant, 2-Teamfight tactics, 3-Legends of Runneterra, 4- League of Legends.
+    // 1- Valorant, 2-Teamfight tactics, 3-Legends of Runneterra, 4- League of Legends.
 
 }

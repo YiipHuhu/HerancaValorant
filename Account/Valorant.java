5a103b11e347d314581e47fbffe1468c7afa8eb6
Alan Gabriel Giacomoni Przyvara <88240260+YiipHuhu@users.noreply.github.com>
2024-04-30 21:06:16 -0300

    Add files via upload

---
+++ Account/Valorant.java
@@ -0,0 +1,10 @@
+package com.Unipar.HerancaValorant.Account;
+
+import com.Unipar.HerancaValorant.Game.BibliotecaDeAgentes;
+
+public class Valorant extends RiotAccount {
+    private BibliotecaDeAgentes agentesDisponivel;
+    private int passeDeBatalha;
+    private int VP;//moedas
+    private int Radianita;//moedas
+}
---
+++ Game/BibliotecaDeAgentes.java
@@ -0,0 +1,10 @@
+package com.Unipar.HerancaValorant.Game;
+
+public class BibliotecaDeAgentes extends Agentes {
+    private String agentesDisponivel;
+    private float valorAgente;// valor para compra sem contrato
+    private int contrato; //Liberacao de agentes e cosmeticos
+    private boolean XboxGamePass; //caso SIM libera todos os agentes
+
+
+}

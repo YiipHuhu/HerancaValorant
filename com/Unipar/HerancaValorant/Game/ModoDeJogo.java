91542dd1a39432f50ac1e623cfa6f566102c848c
Alan Gabriel Giacomoni Przyvara <88240260+YiipHuhu@users.noreply.github.com>
2024-05-02 20:31:37 -0300

    Update ModoDeJogo.java
    Signed-off-by: Alan Gabriel Giacomoni Przyvara <88240260+YiipHuhu@users.noreply.github.com>

--- com/Unipar/HerancaValorant/Game/ModoDeJogo.java
+++ com/Unipar/HerancaValorant/Game/ModoDeJogo.java
@@ -7,4 +7,36 @@ public class ModoDeJogo extends Valorant {
     private int jogadorPorPartida;
     private int rounds; //numero de rodadas
     private int fimPartida; //modo de fim de partida, pode ser por 1- morte subta, 2- diferença de 2 pontos.
+
+        public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getJogadorPorPartida() {
+        return jogadorPorPartida;
+    }
+
+    public void setJogadorPorPartida(int jogadorPorPartida) {
+        this.jogadorPorPartida = jogadorPorPartida;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public int getFimPartida() {
+        return fimPartida;
+    }
+
+    public void setFimPartida(int fimPartida) {
+        this.fimPartida = fimPartida;
+    }
 }

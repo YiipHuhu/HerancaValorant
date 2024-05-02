579801c395f00146fe6a3f715f7be1ed165e909c
Alan Gabriel Giacomoni Przyvara <88240260+YiipHuhu@users.noreply.github.com>
2024-05-02 20:30:57 -0300

    Update BibliotecaDeAgentes.java
    Signed-off-by: Alan Gabriel Giacomoni Przyvara <88240260+YiipHuhu@users.noreply.github.com>

--- com/Unipar/HerancaValorant/Game/BibliotecaDeAgentes.java
+++ com/Unipar/HerancaValorant/Game/BibliotecaDeAgentes.java
@@ -6,5 +6,35 @@ public class BibliotecaDeAgentes extends Agentes {
     private int contrato; //Liberacao de agentes e cosmeticos
     private boolean XboxGamePass; //caso SIM libera todos os agentes
 
+    public String getAgentesDisponivel() {
+        return agentesDisponivel;
+    }
 
+    public void setAgentesDisponivel(String agentesDisponivel) {
+        this.agentesDisponivel = agentesDisponivel;
+    }
+
+    public float getValorAgente() {
+        return valorAgente;
+    }
+
+    public void setValorAgente(float valorAgente) {
+        this.valorAgente = valorAgente;
+    }
+
+    public int getContrato() {
+        return contrato;
+    }
+
+    public void setContrato(int contrato) {
+        this.contrato = contrato;
+    }
+
+    public boolean isXboxGamePass() {
+        return XboxGamePass;
+    }
+
+    public void setXboxGamePass(boolean xboxGamePass) {
+        XboxGamePass = xboxGamePass;
+    }
 }

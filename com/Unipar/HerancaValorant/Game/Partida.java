ade845ae2d23b802e6933576d76f78dee7bdee22
Alan Gabriel Giacomoni Przyvara <88240260+YiipHuhu@users.noreply.github.com>
2024-05-02 20:32:32 -0300

    Update Partida.java
    Signed-off-by: Alan Gabriel Giacomoni Przyvara <88240260+YiipHuhu@users.noreply.github.com>

--- com/Unipar/HerancaValorant/Game/Partida.java
+++ com/Unipar/HerancaValorant/Game/Partida.java
@@ -7,4 +7,26 @@ public class Partida extends ModoDeJogo {
     private Mapas nome;
     private BibliotecaDeAgentes agentesDisponivel;
     private ModoDeJogo tipo;
+
+    public Mapas getNome() {
+        return nome;
+    }
+
+    public void setNome(Mapas nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public BibliotecaDeAgentes getAgentesDisponivel() {
+        return agentesDisponivel;
+    }
+
+    @Override
+    public void setAgentesDisponivel(BibliotecaDeAgentes agentesDisponivel) {
+        this.agentesDisponivel = agentesDisponivel;
+    }
+
+    public void setTipo(ModoDeJogo tipo) {
+        this.tipo = tipo;
+    }
 }

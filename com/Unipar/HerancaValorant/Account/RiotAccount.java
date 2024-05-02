6763d2d6f57585eab2e590b8d65073b461c96d96
Alan Gabriel Giacomoni Przyvara <88240260+YiipHuhu@users.noreply.github.com>
2024-05-02 20:29:18 -0300

    Update RiotAccount.java
    Signed-off-by: Alan Gabriel Giacomoni Przyvara <88240260+YiipHuhu@users.noreply.github.com>

--- com/Unipar/HerancaValorant/Account/RiotAccount.java
+++ com/Unipar/HerancaValorant/Account/RiotAccount.java
@@ -4,5 +4,19 @@ public class RiotAccount {
     private String riotID;
     private int jogo;
     // 1- Valorant, 2-Teamfight tactics, 3-Legends of Runneterra, 4- League of Legends.
+    public String getRiotID() {
+        return riotID;
+    }
 
+    public void setRiotID(String riotID) {
+        this.riotID = riotID;
+    }
+
+    public int getJogo() {
+        return jogo;
+    }
+
+    public void setJogo(int jogo) {
+        this.jogo = jogo;
+    }
 }

5043d12acab4d67a66da07d96b5ed77f1ee1fa8d
Alan Gabriel Giacomoni Przyvara <88240260+YiipHuhu@users.noreply.github.com>
2024-05-02 20:31:18 -0300

    Update Mapas.java
    Signed-off-by: Alan Gabriel Giacomoni Przyvara <88240260+YiipHuhu@users.noreply.github.com>

--- com/Unipar/HerancaValorant/Game/Mapas.java
+++ com/Unipar/HerancaValorant/Game/Mapas.java
@@ -5,4 +5,36 @@ public class Mapas {
     private int orbes;
     private int numBombs;
     private int diferencial; //1-portal, 2-rapel, 3-corda, 4-portao
+
+        public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getOrbes() {
+        return orbes;
+    }
+
+    public void setOrbes(int orbes) {
+        this.orbes = orbes;
+    }
+
+    public int getNumBombs() {
+        return numBombs;
+    }
+
+    public void setNumBombs(int numBombs) {
+        this.numBombs = numBombs;
+    }
+
+    public int getDiferencial() {
+        return diferencial;
+    }
+
+    public void setDiferencial(int diferencial) {
+        this.diferencial = diferencial;
+    }
 }

81a90358b620478f71cedb06fadc13ef51d8a2ee
Alan Gabriel Giacomoni Przyvara <88240260+YiipHuhu@users.noreply.github.com>
2024-05-02 20:29:48 -0300

    Update Valorant.java
    Signed-off-by: Alan Gabriel Giacomoni Przyvara <88240260+YiipHuhu@users.noreply.github.com>

--- com/Unipar/HerancaValorant/Account/Valorant.java
+++ com/Unipar/HerancaValorant/Account/Valorant.java
@@ -7,4 +7,35 @@ public class Valorant extends RiotAccount {
     private int passeDeBatalha;
     private int VP;//moedas
     private int Radianita;//moedas
+        public BibliotecaDeAgentes getAgentesDisponivel() {
+        return agentesDisponivel;
+    }
+
+    public void setAgentesDisponivel(BibliotecaDeAgentes agentesDisponivel) {
+        this.agentesDisponivel = agentesDisponivel;
+    }
+
+    public int getPasseDeBatalha() {
+        return passeDeBatalha;
+    }
+
+    public void setPasseDeBatalha(int passeDeBatalha) {
+        this.passeDeBatalha = passeDeBatalha;
+    }
+
+    public int getVP() {
+        return VP;
+    }
+
+    public void setVP(int VP) {
+        this.VP = VP;
+    }
+
+    public int getRadianita() {
+        return Radianita;
+    }
+
+    public void setRadianita(int radianita) {
+        Radianita = radianita;
+    }
 }
